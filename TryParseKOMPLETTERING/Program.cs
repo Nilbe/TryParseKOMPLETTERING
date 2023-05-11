e18049c91fc03da5ebd5dc05df0f4e971c19ca44
Nilbe <90683548+Nilbe@users.noreply.github.com>
2023-05-11 10:19:29 +0200

    commit1

---
+++ TryParseKOMPLETTERING/Program.cs
@@ -0,0 +1,28 @@
+﻿int answer = 0;
+
+bool stringToInt = false;
+
+while(stringToInt != true)
+{
+    Console.WriteLine("What is 1+1?");
+    Console.WriteLine("Enter your answer");
+    string input = Console.ReadLine();
+
+    stringToInt = int.TryParse(input, out answer);
+}
+
+if(stringToInt == true)
+{
+    if(answer == 2)
+    {
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine("False!");
+    }
+
+    Console.WriteLine("Press Enter to close");
+}
+
+Console.ReadLine();
